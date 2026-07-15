e7cfd885dd7d7e910a81681867ae1e5d7c603646
nspclpowerupquiz <vishwakarma.preeti@gmail.com>
2026-07-15 14:48:25 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,8 +1,41 @@
 const questions = [
-  {
-    question: "Which planet is known as the Red Planet?",
-    options: ["Earth", "Mars", "Venus", "Jupiter"],
-    answer: "Mars"
-  },
-  ...
+
+{
+question:"Which planet is known as the Red Planet?",
+options:[
+"Earth",
+"Mars",
+"Jupiter",
+"Venus"
+],
+answer:"Mars",
+category:"Science"
+},
+
+
+{
+question:"What is the national animal of India?",
+options:[
+"Lion",
+"Tiger",
+"Elephant",
+"Peacock"
+],
+answer:"Tiger",
+category:"GK"
+},
+
+
+{
+question:"NSPCL is a joint venture between NTPC and?",
+options:[
+"SAIL",
+"BHEL",
+"GAIL",
+"ONGC"
+],
+answer:"SAIL",
+category:"NSPCL"
+}
+
 ];
