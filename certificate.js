a21779504de80b3f56f390b28e6c3076981bb0ae
nspclpowerupquiz <vishwakarma.preeti@gmail.com>
2026-07-15 12:49:12 +0530

    Update certificate.js

--- certificate.js
+++ certificate.js
@@ -1,1 +1,70 @@
+// =================================
+// NSPCL Certificate Generator
+// =================================
 
+
+
+let name =
+
+localStorage.getItem("NSPCL_User")
+||
+"Participant";
+
+
+
+let leaderboard =
+
+JSON.parse(
+
+localStorage.getItem("leaderboard")
+
+)||[];
+
+
+
+
+let latest =
+
+leaderboard[leaderboard.length-1];
+
+
+
+
+
+document.getElementById(
+"certificateName"
+).innerHTML=name;
+
+
+
+
+
+if(latest){
+
+
+document.getElementById(
+"certificateScore"
+).innerHTML=
+
+`
+Score: ${latest.score} <br>
+Percentage: ${latest.percentage}%
+`;
+
+
+
+}
+
+
+
+
+
+document.getElementById(
+"certificateDate"
+).innerHTML=
+
+"Completed on: "
+
++
+
+new Date().toLocaleDateString();
