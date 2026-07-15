68952d711b29d557a3b484ca608f9c589ada71e5
nspclpowerupquiz <vishwakarma.preeti@gmail.com>
2026-07-15 11:18:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,102 @@
+// ===============================
+// Quiz Questions
+// ===============================
 
+const questions = [
+
+{
+    question: "Which planet is known as the Red Planet?",
+    options: ["Earth","Mars","Jupiter","Venus"],
+    answer: "Mars",
+    category: "Science"
+},
+
+{
+    question: "What is the capital of India?",
+    options: ["Mumbai","Delhi","Kolkata","Chennai"],
+    answer: "Delhi",
+    category: "GK"
+},
+
+{
+    question: "5 × 8 = ?",
+    options: ["35","40","45","50"],
+    answer: "40",
+    category: "Maths"
+},
+
+{
+    question: "Who wrote Harry Potter?",
+    options: [
+        "J.K. Rowling",
+        "Ruskin Bond",
+        "Roald Dahl",
+        "Enid Blyton"
+    ],
+    answer: "J.K. Rowling",
+    category: "Reading"
+},
+
+{
+    question: "How many squares are there on a chessboard?",
+    options: ["32","64","100","81"],
+    answer: "64",
+    category: "Chess"
+},
+
+{
+    question: "Which gas do plants absorb?",
+    options: [
+        "Oxygen",
+        "Nitrogen",
+        "Carbon Dioxide",
+        "Hydrogen"
+    ],
+    answer: "Carbon Dioxide",
+    category: "Science"
+},
+
+{
+    question: "Who is known as the Father of the Nation in India?",
+    options: [
+        "Jawaharlal Nehru",
+        "Subhas Chandra Bose",
+        "Mahatma Gandhi",
+        "Sardar Patel"
+    ],
+    answer: "Mahatma Gandhi",
+    category: "GK"
+},
+
+{
+    question: "12 + 18 = ?",
+    options: ["28","30","32","34"],
+    answer: "30",
+    category: "Maths"
+},
+
+{
+    question: "Who is the author of Charlotte's Web?",
+    options: [
+        "E. B. White",
+        "Ruskin Bond",
+        "Dr. Seuss",
+        "Enid Blyton"
+    ],
+    answer: "E. B. White",
+    category: "Reading"
+},
+
+{
+    question: "Which chess piece can jump over other pieces?",
+    options: [
+        "Queen",
+        "Knight",
+        "Rook",
+        "Bishop"
+    ],
+    answer: "Knight",
+    category: "Chess"
+}
+
+];
