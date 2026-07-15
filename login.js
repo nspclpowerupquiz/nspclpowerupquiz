7576946b40e29a021a3f3ef4a467c443b89a7c0e
nspclpowerupquiz <vishwakarma.preeti@gmail.com>
2026-07-15 17:25:55 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,59 @@
+function login(){
 
+
+let employeeId =
+document.getElementById("employeeId").value;
+
+
+let password =
+document.getElementById("password").value;
+
+
+
+if(employeeId==="" || password===""){
+
+
+document.getElementById("message").innerHTML =
+"⚠️ Please enter Employee ID and Password";
+
+
+return;
+
+
+}
+
+
+
+// Employee ID = Password
+
+if(employeeId === password){
+
+
+
+localStorage.setItem(
+"employee",
+employeeId
+);
+
+
+
+alert("✅ Login Successful");
+
+
+window.location.href="quiz.html";
+
+
+}
+
+else{
+
+
+document.getElementById("message").innerHTML =
+"❌ Invalid Employee ID or Password";
+
+
+}
+
+
+
+}
