8a5a9dd644bbe26c876ad103e8a12041dce20fcc
nspclpowerupquiz <vishwakarma.preeti@gmail.com>
2026-07-15 12:47:26 +0530

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,72 @@
+// =====================================
+// NSPCL POWER-UP QUIZ
+// Leaderboard System
+// =====================================
 
+
+const leaderboardTable = document.getElementById("leaderboardTable");
+
+
+function loadLeaderboard(){
+
+
+    let data = JSON.parse(
+
+        localStorage.getItem("leaderboard")
+
+    ) || [];
+
+
+
+    // Sort highest percentage first
+
+    data.sort((a,b)=>{
+
+        return b.percentage - a.percentage;
+
+    });
+
+
+
+    leaderboardTable.innerHTML="";
+
+
+
+    data.forEach((player,index)=>{
+
+
+        let row=document.createElement("tr");
+
+
+
+        row.innerHTML=`
+
+        <td>${index+1}</td>
+
+        <td>${player.name}</td>
+
+        <td>${player.id}</td>
+
+        <td>${player.score}</td>
+
+        <td>${player.percentage}%</td>
+
+        <td>${player.date}</td>
+
+        `;
+
+
+
+        leaderboardTable.appendChild(row);
+
+
+
+    });
+
+
+
+}
+
+
+
+loadLeaderboard();
