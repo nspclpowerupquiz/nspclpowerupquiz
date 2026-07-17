d4f08a64aba2d725dc20c014f3922bcdbe7e5f0b
nspclpowerupquiz <vishwakarma.preeti@gmail.com>
2026-07-17 15:43:18 +0530

    Update participant.js

--- participant.js
+++ participant.js
@@ -1,1 +1,20 @@
+function saveParticipant(){
 
+    let name = document.getElementById("participantName").value.trim();
+
+
+    if(name==""){
+
+        alert("Please enter your name");
+
+        return;
+
+    }
+
+
+    localStorage.setItem("employeeName", name);
+
+
+    window.location.href="quiz.html";
+
+}
