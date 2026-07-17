13e3119e357b9944683e696645890cc0936f681d
nspclpowerupquiz <vishwakarma.preeti@gmail.com>
2026-07-17 17:49:07 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -21,7 +21,7 @@ let employeeName = localStorage.getItem("employeeName");
 
 // Google Sheet Web App URL
 
-const sheetURL = "https://script.google.com/macros/s/AKfycbxaFLlnAhREGmXNi7YJtpSojqZKujF-MPr_7jvToyEohlKckrdm_f5-jhDA5JBwfTFqXg/exec";
+const sheetURL = https://script.google.com/macros/s/AKfycbwBbn0mA_VbQG3A4lz7nDGWZm66P6jKBx12zXbYZ-OoCudVBzIvK-MkuZEXxLcECl5wdw/exec
 
 
 
