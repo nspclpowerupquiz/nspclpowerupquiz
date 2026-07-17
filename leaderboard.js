d25b764dff3554a16857616e2f026a811c73927b
nspclpowerupquiz <vishwakarma.preeti@gmail.com>
2026-07-17 17:13:56 +0530

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -30,23 +30,16 @@ function loadLeaderboard() {
                 `${data[2].employeeName}<br>Score: ${data[2].score}`;
         }
 
-        data.forEach((player, index) => {
+       data.forEach((player, index) => {
 
-            table.innerHTML += `
-            <tr>
-                <td>${index + 1}</td>
-                <td>${player.employeeId}</td>
-                <td>${player.employeeName}</td>
-                <td>${player.score}</td>
-                <td>${player.totalQuestions}</td>
-                <td>${Math.round(player.percentage * 100)}%</td>
-                <td>${new Date(player.dateTime).toLocaleString()}</td>
-            </tr>`;
-        });
+    const row = table.insertRow();
 
-    })
-    .catch(error => {
-        console.error("Leaderboard Error:", error);
-    });
+    row.insertCell(0).textContent = index + 1;
+    row.insertCell(1).textContent = player.employeeId;
+    row.insertCell(2).textContent = player.employeeName;
+    row.insertCell(3).textContent = player.score;
+    row.insertCell(4).textContent = player.totalQuestions;
+    row.insertCell(5).textContent = Math.round(player.percentage * 100) + "%";
+    row.insertCell(6).textContent = new Date(player.dateTime).toLocaleString();
 
-}
+});        
