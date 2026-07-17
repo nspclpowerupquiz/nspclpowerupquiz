d470b412362a27cce5dea4c902090403689af71e
nspclpowerupquiz <vishwakarma.preeti@gmail.com>
2026-07-17 16:26:55 +0530

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,10 +1,8 @@
 // =====================================
 // NSPCL POWER-UP QUIZ
-// GOOGLE SHEET LEADERBOARD SYSTEM
+// Dynamic Leaderboard
 // =====================================
 
-
-// Google Apps Script URL
 
 const leaderboardURL =
 "https://script.google.com/macros/s/AKfycbxaFLlnAhREGmXNi7YJtpSojqZKujF-MPr_7jvToyEohlKckrdm_f5-jhDA5JBwfTFqXg/exec?action=leaderboard";
@@ -15,15 +13,10 @@ const leaderboardTable =
 document.getElementById("leaderboardTable");
 
 
-
 const topThree =
 document.getElementById("topThree");
 
 
-
-
-
-function loadLeaderboard(){
 
 
 
@@ -37,30 +30,19 @@ fetch(leaderboardURL)
 
 
 
-    // Clear loading text
-
-    topThree.innerHTML="";
+    topThree.innerHTML = "";
 
 
-
-    // Clear old rows except header
 
     leaderboardTable.innerHTML = `
 
     <tr>
-
     <th>Rank</th>
-
     <th>Employee Name</th>
-
     <th>Score</th>
-
     <th>Total Questions</th>
-
     <th>Percentage</th>
-
     <th>Date</th>
-
     </tr>
 
     `;
@@ -68,8 +50,11 @@ fetch(leaderboardURL)
 
 
 
-
     data.forEach(function(player,index){
+
+
+
+        let percentage = Math.round(player.percentage * 100);
 
 
 
@@ -83,17 +68,15 @@ fetch(leaderboardURL)
 
             topThree.innerHTML += `
 
-
             <div class="winner">
 
             <h1>${medal}</h1>
 
             <h2>${player.employeeName}</h2>
 
-            <h3>${player.percentage}</h3>
+            <h3>${percentage}%</h3>
 
             </div>
-
 
             `;
 
@@ -104,40 +87,27 @@ fetch(leaderboardURL)
 
 
 
-
         // Table rows
 
-        let row = document.createElement("tr");
+        let row = leaderboardTable.insertRow();
 
 
 
         row.innerHTML = `
 
-
         <td>${player.rank}</td>
-
 
         <td>${player.employeeName}</td>
 
-
         <td>${player.score}</td>
-
 
         <td>${player.totalQuestions}</td>
 
+        <td>${percentage}%</td>
 
-        <td>${player.percentage}</td>
-
-
-        <td>${player.dateTime}</td>
-
-
+        <td>${new Date(player.dateTime).toLocaleDateString()}</td>
 
         `;
-
-
-
-        leaderboardTable.appendChild(row);
 
 
 
@@ -150,18 +120,6 @@ fetch(leaderboardURL)
 
 .catch(error=>{
 
-
 console.log("Leaderboard Error:",error);
 
-
 });
-
-
-
-}
-
-
-
-
-
-loadLeaderboard();
