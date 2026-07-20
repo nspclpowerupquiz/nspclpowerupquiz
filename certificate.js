c2f702fe775698bb783654a1954d72b0b2744dc6
nspclpowerupquiz <vishwakarma.preeti@gmail.com>
2026-07-20 09:58:23 +0530

    Update certificate.js

--- certificate.js
+++ certificate.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL="YOUR_SCRIPT_URL";
+const SCRIPT_URL="https://script.google.com/macros/s/AKfycbwBbn0mA_VbQG3A4lz7nDGWZm66P6jKBx12zXbYZ-OoCudVBzIvK-MkuZEXxLcECl5wdw/exec";
 
 let employeeId=localStorage.getItem("employeeId");
 
